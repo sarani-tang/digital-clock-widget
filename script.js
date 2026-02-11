d5783570a50b5924130ad097727f9510be175da8
Sarani Tang <sarani.tang2@gmail.com>
2026-02-10 16:45:36 -0800

    added updateTime() function

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function updateDate() {
     const now = new Date();
-    const days = ["sun", "mon", "tues", "wed", "thu", "fri", "sat"];
-    const months = ["jan", "feb", "mar", "apr", "may", "jun", "jul", "aug", "sep", "oct", "nov", "dec"];
+    const days = ["Sun", "Mon", "Tues", "Wed", "Thu", "Fri", "Sat"];
+    const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
 
     const dayName = days[now.getDay()];
     const monthName = months[now.getMonth()];
@@ -11,12 +11,27 @@ function updateDate() {
 
 }
 
+function updateTime(){
+    const now = new Date();
+    let hours = now.getHours();
+    let minutes = now.getMinutes();
+
+    hours = hours % 12;
+    hours = hours ? hours : 12;
+
+    /* hours = hours < 10 ? '0' + hours : hours;*/
+    minutes = minutes < 10 ? '0' + minutes : minutes;
+
+    return `${hours}:${minutes}`;
+}
+
 document.getElementById('date').textContent = updateDate();
+document.getElementById('time').textContent = updateTime();
 
 setInterval(() => {
     document.getElementById('date').textContent = updateDate();
 }, 60000); //checks every minute and updates automatically at midnight
 
-function updateTime(){
-
-}
+setInterval(() => {
+    document.getElementById('time').textContent = updateTime();
+}, 1000);
